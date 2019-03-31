369648fc01d4b6099100e59e311291a1d48a16c3
Tom Berghuis <thomas.berghuis@gmail.com>
2019-03-31 15:59:42 +1100

    .

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Header, Label, Input, Grid, Form, Button } from 'semantic-ui-react';
 import Isemail from 'isemail';
 import isEmpty from 'lodash/isEmpty';
 import trim from 'lodash/trim';
 import axios from 'axios';
 
-class Register extends Component {
-	state = {
-		formErrors: {}
-	};
+export default function Register({ history }) {
+	const [ formErrors, setFormErrors ] = useState({});
 
-	handleSubmit = async (e) => {
+	// what would useState with no initial arg do?
+	const [ emailRef, setEmailRef ] = useState(null);
+	const [ passwordRef, setPasswordRef ] = useState(null);
+	const [ passwordConfirmRef, setPasswordConfirmRef ] = useState(null);
+
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-		const email = trim(this.email.inputRef.current.value);
-		const password = this.password.inputRef.current.value;
-		const password2 = this.password2.inputRef.current.value;
+		const email = trim(emailRef.inputRef.current.value);
+		const password = passwordRef.inputRef.current.value;
+		const password2 = passwordConfirmRef.inputRef.current.value;
 
 		let formErrors = {};
 		// validation
@@ -34,7 +37,7 @@ class Register extends Component {
 		}
 
 		if (!isEmpty(formErrors)) {
-			this.setState({ formErrors });
+			setFormErrors(formErrors);
 			return;
 		}
 
@@ -42,7 +45,7 @@ class Register extends Component {
 
 		try {
 			await axios.post('/api/users/register', userData);
-			this.props.history.push('/login');
+			history.push('/login');
 		} catch (resErr) {
 			console.log('resErr', resErr.response);
 			// server validation failed, email already exists
@@ -51,58 +54,50 @@ class Register extends Component {
 		}
 	};
 
-	render() {
-		return (
-			<Grid centered>
-				<Grid.Row>
-					<Grid.Column mobile={16} tablet={8}>
-						<Header textAlign="center" as="h1">
-							Sign up
-						</Header>
-						<Form noValidate onSubmit={this.handleSubmit}>
-							<Form.Field error={!!this.state.formErrors.email}>
-								<label>Email</label>
-								<Input
-									ref={(input) => (this.email = input)}
-									icon="user"
-									iconPosition="left"
-									placeholder="Email"
-								/>
-								{!!this.state.formErrors.email && <Label pointing>{this.state.formErrors.email}</Label>}
-							</Form.Field>
-							<Form.Field error={!!this.state.formErrors.password}>
-								<label>Password</label>
-								<Input
-									ref={(input) => (this.password = input)}
-									type="password"
-									icon="lock"
-									iconPosition="left"
-									placeholder="Password"
-								/>
-								{!!this.state.formErrors.password && (
-									<Label pointing>{this.state.formErrors.password}</Label>
-								)}
-							</Form.Field>
-							<Form.Field error={!!this.state.formErrors.password2}>
-								<label>Confirm Password</label>
-								<Input
-									ref={(input) => (this.password2 = input)}
-									type="password"
-									icon="lock"
-									iconPosition="left"
-									placeholder="Password"
-								/>
-								{!!this.state.formErrors.password2 && (
-									<Label pointing>{this.state.formErrors.password2}</Label>
-								)}
-							</Form.Field>
-							<Button>Sign-up</Button>
-						</Form>
-					</Grid.Column>
-				</Grid.Row>
-			</Grid>
-		);
-	}
+	return (
+		<Grid centered>
+			<Grid.Row>
+				<Grid.Column mobile={16} tablet={8}>
+					<Header textAlign="center" as="h1">
+						Sign up
+					</Header>
+					<Form noValidate onSubmit={handleSubmit}>
+						<Form.Field error={!!formErrors.email}>
+							<label>Email</label>
+							<Input
+								ref={(input) => setEmailRef(input)}
+								icon="user"
+								iconPosition="left"
+								placeholder="Email"
+							/>
+							{!!formErrors.email && <Label pointing>{formErrors.email}</Label>}
+						</Form.Field>
+						<Form.Field error={!!formErrors.password}>
+							<label>Password</label>
+							<Input
+								ref={(input) => setPasswordRef(input)}
+								type="password"
+								icon="lock"
+								iconPosition="left"
+								placeholder="Password"
+							/>
+							{!!formErrors.password && <Label pointing>{formErrors.password}</Label>}
+						</Form.Field>
+						<Form.Field error={!!formErrors.password2}>
+							<label>Confirm Password</label>
+							<Input
+								ref={(input) => setPasswordConfirmRef(input)}
+								type="password"
+								icon="lock"
+								iconPosition="left"
+								placeholder="Password"
+							/>
+							{!!formErrors.password2 && <Label pointing>{formErrors.password2}</Label>}
+						</Form.Field>
+						<Button>Sign-up</Button>
+					</Form>
+				</Grid.Column>
+			</Grid.Row>
+		</Grid>
+	);
 }
-
-export default Register;
