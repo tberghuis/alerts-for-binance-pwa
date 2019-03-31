e9b502c8025e9443ee4c656e5540697a6a5522bd
Tom Berghuis <thomas.berghuis@gmail.com>
2019-03-31 16:44:19 +1100

    .

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import alertsList from './alertsList';
-import homescreen from './homescreen';
 
 export default combineReducers({
 	auth,
-	alertsList,
-	homescreen
+	alertsList
 });
