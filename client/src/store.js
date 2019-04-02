d6d8cfeafd75fcee62347ec9b82efe08dacf3c3a
Tom Berghuis <thomas.berghuis@gmail.com>
2019-04-02 13:18:19 +1100

    .

--- client/src/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux';
-import rootReducer from './reducers';
-
-export const store = createStore(
-	rootReducer /* preloadedState, */,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-export const dispatch = store.dispatch;
